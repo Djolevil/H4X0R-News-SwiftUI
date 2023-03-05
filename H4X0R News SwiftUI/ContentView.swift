536a0349c995bb2f8a71d7a833a49d89df4d6514
Djordje Knezevic <starwarrior@mycity.rs>
2023-03-05 18:58:19 +0100

    Use Observer Design Pattern to display data received from HackerNews API

--- H4X0R News SwiftUI/ContentView.swift
+++ H4X0R News SwiftUI/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
             .navigationTitle("H4X0R NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
         }
     }
 }
@@ -23,9 +32,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-let posts = [
-    Post(id: "1", title: "First Post"),
-    Post(id: "2", title: "Second Post"),
-    Post(id: "3", title: "Third Post"),
-]
