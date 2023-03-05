4dbe7982f0b39c7cdd01b7a57b78f2b00168be02
Djordje Knezevic <starwarrior@mycity.rs>
2023-03-05 15:20:46 +0100

    Use URLSession and JSONDecoder to fetch news data from HackerNews API

--- H4X0R News SwiftUI/ContentView.swift
+++ H4X0R News SwiftUI/ContentView.swift
@@ -24,11 +24,6 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Post: Identifiable {
-  let id: String
-    let title: String
-}
-
 let posts = [
     Post(id: "1", title: "First Post"),
     Post(id: "2", title: "Second Post"),
